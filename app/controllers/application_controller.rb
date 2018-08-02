690befe7b8fc1497a40ef72cbabaae188554aeb6
jcsansom <jayceesansom@gmail.com>
2018-08-02 17:39:39 +0000

    Finished back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,43 +33,43 @@ class ApplicationController < Sinatra::Base
     erb :food
   end
   
-  post "/fitness"
+  post "/fitness" do
     erb :fitness
   end
 
-  post "/health_2"
+  post "/health_2" do
     erb :health_2
   end
 
-  post "/cooking"
+  post "/cooking" do
     erb :cooking
   end
 
-  post "/reviews"
+  post "/reviews" do
     erb :reviews
   end
 
-  post "/bullying"
+  post "/bullying" do
     erb :bullying
   end
 
-  post "/misunderstood"
+  post "/misunderstood" do
     erb :misunderstood
   end
 
-  post "/fashion"
+  post "/fashion" do
     erb :fashion
   end
 
-  post "/sports"
+  post "/sports" do
     erb :sports
   end
 
-  post "/other"
+  post "/other" do
     erb :other
   end
 
-  post "/help"
+  post "/help" do 
     erb :help
   end
   
